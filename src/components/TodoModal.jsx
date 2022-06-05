becd6cb33af3d78d48550f536300ae29515c7536
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-06-05 17:50:05 +0600

    Add function done

--- src/components/TodoModal.jsx
+++ src/components/TodoModal.jsx
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
 import { MdOutlineClose } from "react-icons/md";
+import { addTodo } from "../slices/todoSlice";
 import styles from "../styles/modules/modal.module.scss";
 import Button from "./Button";
+import { useDispatch } from "react-redux";
+import { v4 as uuid } from "uuid";
+import toast from "react-hot-toast";
 
 const TodoModal = ({ modalOpen, setModalOpen }) => {
   const [title, setTitle] = useState("");
   const [status, setStatus] = useState("incomplete");
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ title, status });
+    if (title && status) {
+      dispatch(
+        addTodo({
+          id: uuid,
+          title,
+          status,
+          time: new Date().toLocaleDateString(),
+        })
+      );
+      toast.success("Task Added Successfully");
+      setModalOpen(false);
+    } else {
+      toast.error("Title shouldn't be empty");
+    }
   };
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./styles/GlobalStyles.css";
 import "@fontsource/poppins";
 import "@fontsource/poppins/500.css";
 import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
+import { store } from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
