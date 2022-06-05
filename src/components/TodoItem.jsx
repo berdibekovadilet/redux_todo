fb2059b7d1f1b454ccb0fd5364f5151d3ebdbea9
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-06-05 19:51:40 +0600

    added delete func

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,8 +3,20 @@ import React from "react";
 import styles from "../styles/modules/todoItem.module.scss";
 import { getClasses } from "../utils/getClasses";
 import { MdDelete, MdEdit } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { deleteTodo } from "../slices/todoSlice";
 
 const TodoItem = ({ todo }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    dispatch(deleteTodo(todo.id));
+  };
+
+  const handleUpdate = (id) => {
+    console.log("updating");
+  };
+
   return (
     <div className={styles.item}>
       <div className={styles.todoDetails}>
@@ -24,10 +36,22 @@ const TodoItem = ({ todo }) => {
         </div>
       </div>
       <div className={styles.todoActions}>
-        <div className={styles.icon}>
+        <div
+          className={styles.icon}
+          onClick={() => handleDelete()}
+          onKeyDown={() => handleDelete()}
+          tabIndex={0}
+          role="button"
+        >
           <MdDelete />
         </div>
-        <div className={styles.icon}>
+        <div
+          className={styles.icon}
+          onClick={() => handleUpdate()}
+          onKeyDown={() => handleUpdate()}
+          tabIndex={0}
+          role="button"
+        >
           <MdEdit />
         </div>
       </div>
